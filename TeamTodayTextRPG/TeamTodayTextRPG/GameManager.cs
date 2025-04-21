7ee9c5cfef2cae0d5e5316801dc6213602b6840f
garamzui <forsave121@gmail.com>
2025-04-21 17:47:55 +0900

    add
    asdasd

--- TeamTodayTextRPG/TeamTodayTextRPG/GameManager.cs
+++ TeamTodayTextRPG/TeamTodayTextRPG/GameManager.cs
@@ -8,7 +8,7 @@ namespace TeamTodayTextRPG
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Worldasdasd!");
         }
     }
 }
