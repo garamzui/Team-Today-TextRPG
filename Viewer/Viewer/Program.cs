a105e78c636ddaa6d6624a5b939d4d4f9d655503
JayJeongJW <p1fixer2@gmail.com>
2025-04-21 19:42:11 +0900

    코드 추가
    코드추가

--- Viewer/Viewer/Program.cs
+++ Viewer/Viewer/Program.cs
@@ -14,9 +14,47 @@
 
 public abstract class Viewer
 {
-    protected int startIndex;
-    protected int endIndex;
-    protected int dungeonCode;
-    public abstract void ViewAction(GameManager);
-    public abstract VIEW_TYPE NextView(GameManager, int);
+    private int startIndex;
+    private int endIndex;
+    private int dungeonCode;
+    public abstract void ViewAction(GameManager gameManager);
+    public abstract VIEW_TYPE NextView(GameManager gameManager, int input);
 }
+
+public class MainViewer : Viewer
+{
+    public override void ViewAction(GameManager gameManager)
+    {
+        Console.WriteLine("게임 시작 화면");
+        Console.WriteLine("1. 상태 보기");
+        Console.WriteLine("2. 인벤토리");
+        Console.WriteLine("3. 장비");
+        Console.WriteLine("4. 상점");
+        Console.WriteLine("5. 던전");
+        Console.WriteLine("6. 종료");
+    }
+
+    public override VIEW_TYPE NextView(GameManager gameManager, int input)
+    {
+        switch (input)
+        {
+            case 1:
+                return VIEW_TYPE.STATUS;  // 상태 보기 화면
+            case 2:
+                return VIEW_TYPE.INVENTORY;  // 인벤토리 화면
+            case 3:
+                return VIEW_TYPE.EQUIP;  // 장비 화면
+            case 4:
+                return VIEW_TYPE.SHOP;  // 상점 화면
+            case 5:
+                return VIEW_TYPE.DUNGEON;  // 던전 화면
+            case 6:
+                Environment.Exit(0);  // 게임 종료
+                break;
+            default:
+                break;
+        }
+        return VIEW_TYPE.MAIN;  // 기본적으로 메인 화면으로 돌아감
+    }
+}
+public class StatusViewer : Viewer
