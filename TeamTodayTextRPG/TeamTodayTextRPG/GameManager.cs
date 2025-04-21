75ec8a6b884a87796101ea83cb529417fb70930b
garamzui <forsave121@gmail.com>
2025-04-21 17:34:56 +0900

    add
    project script

---
+++ TeamTodayTextRPG/TeamTodayTextRPG/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using TeamTodayTextRPG;
+
+
+namespace TeamTodayTextRPG
+{
+    internal class GameManager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
