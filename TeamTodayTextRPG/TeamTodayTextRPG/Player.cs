0a5ebe720f5e6c69a9da14c46850ab9698bd8ca9
hkY333 <kimabc0630@naver.com>
2025-04-21 19:32:52 +0900

    변수와 메서드 선언
    변수와 메서드 선언
    (매개변수 이름은 나중에 바꿔야 함)

---
+++ TeamTodayTextRPG/TeamTodayTextRPG/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamTodayTextRPG
+{
+    class Player
+    {
+        private Character character;
+        private List<int> bag;
+        private List<int> equip;
+        private int level;
+        private int exp;
+
+        public void SetCharacter(int stat, string name)
+        {
+
+        }
+
+        public void LevelUp(int requiredExp)
+        {
+
+        }
+
+        public bool CheckBag(int hasBag)
+        {
+
+        }
+
+        public void InputBag(int inBag)
+        {
+
+        }
+
+        public void RemoveBag(int outBag)
+        {
+
+        }
+
+        public bool CheckEquip(int hasEquip)
+        {
+
+        }
+
+        public void EquipItem(int equip)
+        {
+
+        }
+
+        public void UnEquipItem(int unEquip)
+        {
+
+        }
+    }
+}
