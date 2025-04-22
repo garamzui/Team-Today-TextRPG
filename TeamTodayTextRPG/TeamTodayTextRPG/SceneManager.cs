79b966d67f1aed87645fe202c262c82982f1d26c
JayJeongJW <p1fixer2@gmail.com>
2025-04-22 21:29:11 +0900

    내일할일
    내일 수정

--- TeamTodayTextRPG/TeamTodayTextRPG/SceneManager.cs
+++ TeamTodayTextRPG/TeamTodayTextRPG/SceneManager.cs
@@ -3,66 +3,73 @@ using TeamTodayTextRPG;
 
 namespace TeamTodayTextRPG
 {
-    // 각 View 상태를 구분하기 위한 열거형 정의
-    public enum VIEW_TYPE
-    {
-        MAIN,
-        STATUS,
-        INVENTORY,
-        EQUIP,
-        SHOP,
-        PURCHASE,
-        SALE,
-        DUNGEON,
-        DUNGEONCLEAR,
-        REST,
-        BATTLE,
-        MONSTER
-    }
-
-    // 현재 화면(Viewer)을 관리하고 전환하는 클래스
+    //**객체 받고/ 속성 받기** To.재우
+    // SceneManager 클래스: 씬 전환을 관리
     public class SceneManager
     {
-        public Viewer CurrentViewer { get; set; }
+        private Viewer currentViewer;
+        private GameManager gameManager;  // GameManager 객체를 멤버로 추가
 
-        public SceneManager()
+        public SceneManager(GameManager gameManager) //*GameManager 객체를 생성자에서 받아야함
         {
-            // 초기 화면은 MainViewer로 설정
-            CurrentViewer = new MainViewer();
+            this.gameManager = gameManager;  // 생성자에서 gameManager 객체 받기
         }
 
-        // VIEW_TYPE에 따라 Viewer 인스턴스를 전환
         public void SwitchScene(VIEW_TYPE viewType)
         {
-            CurrentViewer = viewType switch
+            // 새로운 뷰어 할당
+            switch (viewType)
             {
-                VIEW_TYPE.MAIN => new MainViewer(),
-                VIEW_TYPE.STATUS => new StatusViewer(),
-                VIEW_TYPE.INVENTORY => new InventoryViewer(),
-                VIEW_TYPE.EQUIP => new EquipViewer(),
-                VIEW_TYPE.SHOP => new ShopViewer(),
-                VIEW_TYPE.PURCHASE => new PurchaseViewer(),
-                VIEW_TYPE.SALE => new SaleViewer(),
-                VIEW_TYPE.DUNGEON => new DungeonViewer(),
-                VIEW_TYPE.DUNGEONCLEAR => new DungeonClearViewer(),
-                VIEW_TYPE.REST => new RestViewer(),
-                VIEW_TYPE.BATTLE => new BattleViewer(),
-                VIEW_TYPE.MONSTER => new MonsterViewer(),
-                _ => throw new ArgumentException("Unknown view type")
-            };
+                case VIEW_TYPE.MAIN:
+                    currentViewer = new MainViewer();
+                    break;
+                case VIEW_TYPE.STATUS:
+                    currentViewer = new StatusViewer();
+                    break;
+                case VIEW_TYPE.INVENTORY:
+                    currentViewer = new InventoryViewer();
+                    break;
+                case VIEW_TYPE.EQUIP:
+                    currentViewer = new EquipViewer();
+                    break;
+                case VIEW_TYPE.SHOP:
+                    currentViewer = new ShopViewer();
+                    break;
+                case VIEW_TYPE.PURCHASE:
+                    currentViewer = new PurchaseViewer();
+                    break;
+                case VIEW_TYPE.SALE:
+                    currentViewer = new SaleViewer();
+                    break;
+                case VIEW_TYPE.DUNGEON:
+                    currentViewer = new DungeonViewer();
+                    break;
+                case VIEW_TYPE.DUNGEONCLEAR:
+                    currentViewer = new DungeonClearViewer();
+                    break;
+                case VIEW_TYPE.REST:
+                    currentViewer = new RestViewer();
+                    break;
+                case VIEW_TYPE.BATTLE:
+                    currentViewer = new BattleViewer();
+                    break;
+                case VIEW_TYPE.MONSTER:
+                    // GameManager에서 직접적으로 몬스터 객체를 가져오는 방식으로 수정
+                    Monster currentMonster = gameManager.CurrentMonster;//*GameManager에서 속성을 추가 후 받아야함
+                    currentViewer = new MonsterViewer(currentMonster);
+                    break;
+            }
+
+            // 새로운 뷰어의 화면 출력
+            ShowCurrentView();
         }
 
-        // 현재 Viewer의 화면을 출력
-        public void ShowCurrentView(GameManager gameManager)
+        public void ShowCurrentView()
         {
-            CurrentViewer?.ViewAction(gameManager);
-        }
-
-        // 입력을 받아 다음 화면으로 전환
-        public void HandleInput(GameManager gameManager, int input)
-        {
-            VIEW_TYPE nextView = CurrentViewer.NextView(gameManager, input);
-            SwitchScene(nextView);
+            if (currentViewer != null)
+            {
+                currentViewer.ViewAction(gameManager);  // gameManager 객체를 넘기기
+            }
         }
     }
 }
