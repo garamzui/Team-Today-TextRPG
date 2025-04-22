68e8845f51fdb1405b5cb6ad3d45dacc4d35306d
hkY333 <kimabc0630@naver.com>
2025-04-22 10:00:34 +0900

    매개변수 다시 선언, level++ 추가, 주석 추가

--- TeamTodayTextRPG/TeamTodayTextRPG/Player.cs
+++ TeamTodayTextRPG/TeamTodayTextRPG/Player.cs
@@ -15,9 +15,9 @@ namespace TeamTodayTextRPG
         private int level;
         private int exp;
 
+        //Characterclass 스크립트에서 스탯의 정보를 받아온다
         public void SetCharacter()
         {
-            //Characterclass 스크립트에서 스탯의 정보를 받아온다
 
 
         }
@@ -32,7 +32,8 @@ namespace TeamTodayTextRPG
                 //경험치에서 요구 경험치 만큼 빼고 초과량은 현재 경험치로 남는다.
                 exp = exp - requiredExp;
 
-                //요구 경험치가 늘어난다.
+                //레벨 및 요구 경험치가 늘어난다.
+                level++;
                 requiredExp += 25;
 
                 //Console.WriteLine("축하합니다! 레벨이 올랐습니다.");
@@ -40,9 +41,19 @@ namespace TeamTodayTextRPG
             }
 
         }
+        //아이템에 코드값을 받아서 그 아이템이 내 인벤에 있는지 없는지 체크하는 불값
+        //플레이어 bag이라는 리스트가 아이템의 코드번호를 가지고 저장
+        //0~7 아이템 있는데 6번 찾고 싶으면 bag(6)을 입력하면 true 반환 하는 식으로 제작
+        //equipItem의 경우는 같은 장비군은 중복장착하면 안되니까 세부내용 조금 다름
 
-        //인벤토리를 열면 활성화
-        public bool CheckBag = false;
+        //인벤토리에 해당 아이템이 있으면
+        public bool CheckBag(int code)
+        {
+            //bag에 Item의 code를 인덱스로 저장한다
+            
+
+            
+        }
 
         //인벤토리에 아이템이 들어오는 경우
         public void InputBag()
@@ -65,8 +76,11 @@ namespace TeamTodayTextRPG
 
         }
 
-        //장비창을 열면 활성화
-        public bool CheckEquip = false;
+        //인벤토리에 해당 아이템이 있으면
+        public bool CheckEquip(int code)
+        {
+
+        }
 
 
         public void EquipItem()
