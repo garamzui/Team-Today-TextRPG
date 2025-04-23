3b9f5158d648c82491940e3e06aceaadb2dfee1d
kimheejoo0828 <o1043381828@nate.com>
2025-04-23 09:19:45 +0900

    아이템

--- TeamTodayTextRPG/TeamTodayTextRPG/Item.cs
+++ TeamTodayTextRPG/TeamTodayTextRPG/Item.cs
@@ -116,36 +116,97 @@ namespace TeamTodayTextRPG
             if (Def != 0) Console.Write($"Def {(Def >= 0 ? " + " : "")}{Def}");
         }
 
-        public void ShowInventory(GameManager gameManager , VIEW_TYPE vIEW_TYPE)
+        public void ShowInventory(GameManager gameManager, VIEW_TYPE vIEW_TYPE)
         {
 
-            if (Player.Inventory != null)
+            var player = gameManager.Player;
+
+            if (player.Inventory != null)
             {
-                foreach (var item in Player.Inventory)
+                foreach (var code in player.Inventory)
                 {
+                    var item = itemList.FirstOrDefault(i => i.Code == code);
+                    if (item == null) continue;
+
                     Console.Write("- ");
-                    if (Player.CheckEquip(item))
+                    if (player.CheckEquip(item.Code))
                     {
+                        Console.ForegroundColor = ConsoleColor.Green;
                         Console.Write("[E]");
+                        Console.ResetColor();
                     }
+
+                    Console.Write($"{item.Name}\t| ");
+                    if (item.Atk != 0) Console.Write($"공격력 {(item.Atk > 0 ? "+" : "")}{item.Atk}\t| ");
+                    if (item.Def != 0) Console.Write($"방어력 {(item.Def > 0 ? "+" : "")}{item.Def}\t| ");
+                    Console.WriteLine($"{item.Text}");
                 }
             }
         }
 
         public void ShowShop(GameManager gameManager, VIEW_TYPE vIEW_TYPE)
         {
-            if (itemList != null)
-            {
-                foreach (var item in itemList)
-                {
+            var player = gameManager.Player;
 
+            foreach (var item in itemList)
+            {
+                if (player.CheckBag(item.Code)) Console.ForegroundColor = ConsoleColor.DarkGray;
+
+                Console.Write($"- {item.Name}\t| ");
+                if (item.Atk != 0) Console.Write($"공격력 {(item.Atk > 0 ? "+" : "")}{item.Atk}\t| ");
+                if (item.Def != 0) Console.Write($"방어력 {(item.Def > 0 ? "+" : "")}{item.Def}\t| ");
+                Console.Write($"{item.Text}\t| ");
+
+                if (player.CheckBag(item.Code))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("구매 완료");
                 }
+
+                else
+                {
+                    Console.WriteLine($"{item.Value} G");
+                }
+
+                Console.ResetColor();
             }
         }
 
         public void ShowsShopSale(GameManager gameManager)
         {
+            var player = gameManager.Player;
 
+            if (player.Inventory != null)
+            {
+                Console.WriteLine("[판매 목록]");
+
+                foreach (var code in player.Inventory)
+                {
+                    var item = itemList.FirstOrDefault(i => i.Code == code);
+                    if (item == null) continue;
+
+                    Console.Write("- ");
+                    if (player.CheckEquip(item.Code))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("[E]");
+                        Console.ResetColor();
+                    }
+
+                    Console.Write($"{item.Name}\t| ");
+                    if (item.Atk != 0) Console.Write($"공격력 {(item.Atk > 0 ? "+" : "")}{item.Atk}\t| ");
+                    if (item.Def != 0) Console.Write($"방어력 {(item.Def > 0 ? "+" : "")}{item.Def}\t| ");
+                    Console.Write($"{item.Text}\t| ");
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"판매가 {item.Value / 2} G");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.WriteLine("판매할 아이템이 없습니다.");
+            }
         }
             
     }
